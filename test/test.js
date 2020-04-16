9264cf87ede2bffab546886c362df64959e814a6
M1T0zd <34724179+M1T0zd@users.noreply.github.com>
2020-04-16 13:51:09 +0200

    Testing environment setup
    - And some small edits

---
+++ test/test.js
@@ -0,0 +1,33 @@
+webCLI = require("../web-cli.js");
+
+webCLI.setPassword("secret");
+webCLI.setPort("8080");
+webCLI.setLogStatus(true);
+
+webCLI.interpreter(commandHandler);
+
+webCLI.start();
+
+function commandHandler(command, args)
+{
+	if(command === "say")
+	{
+		let response = "";
+		args.forEach(arg => {response += arg + " "});
+		if(args) webCLI.sendLog(response); //Send a message back to the client.
+	}
+	else if(command === "confirm")
+	{
+			//console.log(webCLI.sendProbe("are you sure?")); //ToDo
+	}
+	else if (command === "help")
+	{
+		webCLI.sendLog("List of commands:\n" +
+		"\tsay\n" +
+		"\tconfirm\n");
+	}
+	else
+	{
+		webCLI.sendLog("Invalid command. Try `help`.");
+	}
+}
